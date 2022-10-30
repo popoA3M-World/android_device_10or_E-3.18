5c14bca10c1af918552ccf90e7e6cbe3b9caeedb
ItsVixano <vixanonew@gmail.com>
2022-10-30 09:57:31 +0000

    E: Build camera HAL from source
    * also drop hal1 support
    
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -58,7 +58,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.display.lmax=1280x720 \
     camera.display.umax=1920x1080 \
-    camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
     persist.camera.gyro.android=1 \
     persist.camera.is_type=1 \
     vendor.vidc.enc.narrow.searchrange=1 \
